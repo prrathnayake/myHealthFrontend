9fb2cfa471a4231f7ef96ba8e62e6a2314aab0f6
Pasan Ransika Rathnayake <pasanransika1@gmail.com>
2023-01-15 00:11:07 +0530

    add api connection to get patients screen

--- src/screens/patients_screen/patients_screen.js
+++ src/screens/patients_screen/patients_screen.js
@@ -1,11 +1,20 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import NavBar from "../../components/navBar/navBar";
 import apiEndpoint from "../../utils/api";
 
 export default function PatientsScreen() {
   const navigate = useNavigate();
+  const [patients, setPatients] = useState([]);
+
+  useEffect(() => {
+    const id = JSON.parse(localStorage.getItem("id"));
+    axios
+      .get(`${apiEndpoint}patients/doctorId?id=${id}`)
+      .then((response) => setPatients(response.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   const validate = async (accessToken) => {
     await axios({
@@ -31,9 +40,42 @@ export default function PatientsScreen() {
     if (accessToken === null) return navigate("/login");
     validate(accessToken);
   });
+
+  function calculateAge(bday) {
+    let birthdate = new Date(bday);
+    let diff_ms = Date.now() - birthdate.getTime();
+    let age_dt = new Date(diff_ms);
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+  }
   return (
     <>
       <NavBar />
+      <div>
+        <h1>Patients List</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Phone</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {patients.map((patient) => (
+              <tr key={patient.id}>
+                <td>
+                  {patient.firstName} {patient.lastName}
+                </td>
+                <td>{calculateAge(patient.bdate)}</td>
+
+                <td>{patient.mobile}</td>
+                <td>{patient.address}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
